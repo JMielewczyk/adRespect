1341787f64c29798f9df38ba9ec8f920774c334b
JMielewczyk <jkbmielewczyk@gmail.com>
2023-08-18 20:53:58 +0200

    New functions to fetch images & last section & footer added.

--- src/index.js
+++ src/index.js
@@ -10,34 +10,78 @@ var macyInstance = Macy({
   columns: 3,
 });
 
+let fetchedImages = [];
+
 const btnLoadMoreImg = document.querySelector("#btnLoadMoreImg");
 btnLoadMoreImg.addEventListener("click", async () => {
-  const imagesToLoad = 6;
+  const imagesToLoad = 10;
   for (let i = 0; i < imagesToLoad; i++) {
     const getRandomImg = async () => {
-      const randomImgUrl = fetch("https://picsum.photos/200/300");
+      const randomImgUrl = fetch("https://picsum.photos/400/600");
       const randomImg = await randomImgUrl;
-      return randomImg.url;
+      return { id: randomImg.headers.get("Picsum-ID"), url: randomImg.url };
     };
-    const createElements = (urlToImage) => {
+    const createElements = (imgObject) => {
       const newDiv = document.createElement("div");
       const newImg = document.createElement("img");
       newImg.setAttribute("loading", "lazy");
-      newImg.setAttribute("src", urlToImage);
+      newImg.setAttribute("src", imgObject.url);
       newImg.setAttribute("alt", "");
       newImg.className = "w-full h-full";
+      newImg.id = "fetchedImg";
+      newImg.dataset.id = imgObject.id;
       macyContainer.appendChild(newDiv);
       newDiv.appendChild(newImg);
-      console.log("img");
+      fetchedImages = [...fetchedImages, imgObject];
     };
 
-    const randomImg = await getRandomImg();
-    createElements(randomImg);
+    const randomImgObject = await getRandomImg();
+    createElements(randomImgObject);
   }
   macyInstance.runOnImageLoad(function () {
     macyInstance.recalculate(true);
   }, true);
-  console.log("recalculate");
+});
+
+macyContainer.addEventListener("click", async (e) => {
+  const idImg = "fetchedImg";
+  let fullImgPath = "";
+  if (e.target.id === idImg) {
+    const req = fetch(
+      `https://picsum.photos/id/${e.target.dataset.id}/info`
+    ).then((res) => res.json());
+    const data = await req;
+    fullImgPath = data.download_url;
+  } else {
+    fullImgPath = e.target.src;
+  }
+
+  const background = document.createElement("div");
+  const newContainer = document.createElement("div");
+  const newImg = document.createElement("img");
+  const exitBtn = document.createElement("div");
+
+  background.className =
+    "fixed top-0 left-0 flex justify-center items-center w-screen h-screen z-10 bg-gray-500/75";
+  background.id = "popUpBackground";
+  newContainer.className =
+    " w-[80vw] max-w-[1280px] h-[55vh] relative border-2 border-black bg-white";
+  newImg.className = "w-full h-full object-contain overflow-hidden";
+  exitBtn.className =
+    "absolute cursor-pointer right-0 top-0 rounded-full border-[1px] border-black bg-white w-[25px] h-[25px] flex justify-center items-center -translate-y-[50%] translate-x-[50%]";
+  exitBtn.textContent = "X";
+  exitBtn.id = "exitBtn";
+  newImg.setAttribute("src", fullImgPath);
+  document.body.appendChild(background);
+  background.appendChild(newContainer);
+  newContainer.appendChild(newImg);
+  newContainer.appendChild(exitBtn);
+
+  background.addEventListener("click", (e) => {
+    if (e.target.id === background.id || e.target.id === exitBtn.id) {
+      background.remove();
+    } else return;
+  });
 });
 
 const navOfferContainer = document.querySelector("#navOfferContainer");
